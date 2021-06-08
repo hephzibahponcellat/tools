482ca64d889279f2807387d3f0ed7045234112db
Hephzi <52066666+hephzibahponcellat@users.noreply.github.com>
2021-06-08 20:17:16 +0530

    adding random password generator tool

---
+++ password_generator.py
@@ -0,0 +1,33 @@
+"""
+Random password generator
+
+- Generates a password of length 12
+- Includes capital letters, small letters, numbers and symbols
+"""
+
+__author__ = "Hephzibah Pon Cellat Arulprakash"
+__credits__ = ["Hephzibah Pon Cellat Arulprakash"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Hephzibah Pon Cellat Arulprakash"
+__email__ = "poncellat@gmail.com"
+__status__ = "Production"
+
+
+import secrets
+import string
+
+chars = string.ascii_letters + string.digits + string.punctuation
+passwd_len = 12
+
+while True:
+    psswd = ''.join(secrets.choice(chars) for i in range(passwd_len))
+
+    if (
+        any(c.islower() for c in psswd) &
+        any(c.isupper() for c in psswd) &
+        any(c.isdigit() for c in psswd) &
+        any(c in string.punctuation for c in psswd)
+    ):
+        print(psswd)
+        break
